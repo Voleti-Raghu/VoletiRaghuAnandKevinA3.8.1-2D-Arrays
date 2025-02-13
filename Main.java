665bb20bd4bd36f82cc49bd5fb18ccca3580324e
Voleti-Raghu <voleti6284@mydusd.org>
2025-02-13 13:20:53 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        String[][] board = {
+            {"A", "B"},
+            {"C", "D"},
+            {"E", "F"},
+            {"G", "H"}
+        };
+
+        
+        System.out.println(java.util.Arrays.toString(board));
+
+       
+        System.out.println(java.util.Arrays.toString(board[0]));
+        System.out.println(java.util.Arrays.toString(board[1]));
+        System.out.println(java.util.Arrays.toString(board[2]));
+        System.out.println(java.util.Arrays.toString(board[3]));
+    }
+}
